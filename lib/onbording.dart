e2aac0b5c15b6500dbbf1f393bf3d68ea7d1518b
mohidhafeez <mohidzohaib544@gmail.com>
2023-09-07 15:48:39 +0500

    after smooth transaction

--- lib/onbording.dart
+++ lib/onbording.dart
@@ -26,7 +26,7 @@ class _OnbordingState extends State<Onbording> {
   }
 
   void _navigateToHome() {
-    Navigator.of(context).push(
+    Navigator.of(context).pushReplacement(
       MaterialPageRoute(
         builder: (context) =>
             HomePage(), // Replace 'HomeScreen' with your actual home screen widget
@@ -37,89 +37,101 @@ class _OnbordingState extends State<Onbording> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(
-        controller: _pageController,
-        itemCount: contents.length,
-        onPageChanged: (index) {
-          setState(() {
-            _pageIndex = index;
-          });
-        },
-        itemBuilder: (_, i) {
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                const Spacer(),
-                Image.asset(
-                  contents[i].image,
-                  height: 300,
-                ),
-                Text(
-                  contents[i].title,
-                  style: const TextStyle(
-                      fontSize: 40, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  contents[i].discription,
-                  style: const TextStyle(
-                    fontSize: 18,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ...List.generate(
-                        contents.length,
-                        (index) => Padding(
-                              padding: const EdgeInsets.all(4.0),
-                              child: dotindicator(
-                                isActivated: index == _pageIndex,
-                              ),
-                            )),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  height: 50,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (i < contents.length - 1) {
-                        _pageController.nextPage(
-                          duration: const Duration(milliseconds: 300),
-                          curve: Curves.ease,
-                        );
-                      } else {
-                        _navigateToHome(); // Navigate to the home screen when on the last onboarding screen
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: contents.length,
+              onPageChanged: (index) {
+                setState(() {
+                  _pageIndex = index;
+                });
+              },
+              itemBuilder: (_, i) {
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      const Spacer(),
+                      Image.asset(
+                        contents[i].image,
+                        height: 300,
                       ),
-                    ),
-                    child: Text(
-                      i < contents.length - 1
-                          ? 'NEXT'
-                          : 'GET STARTED', // Change the button text on the last screen
-                      style: const TextStyle(fontSize: 18),
-                    ),
+                      Text(
+                        contents[i].title,
+                        style: const TextStyle(
+                            fontSize: 40, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        contents[i].discription,
+                        style: const TextStyle(
+                          fontSize: 18,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                    ],
                   ),
-                ),
-                const Spacer()
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ...List.generate(
+                    contents.length,
+                    (index) => Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: dotindicator(
+                            isActivated: index == _pageIndex,
+                          ),
+                        )),
               ],
             ),
-          );
-        },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 50,
+            width: 320,
+            child: ElevatedButton(
+              onPressed: () {
+                if (_pageIndex < contents.length - 1) {
+                  _pageController.nextPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.ease,
+                  );
+                } else {
+                  _navigateToHome(); // Navigate to the home screen when on the last onboarding screen
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: Text(
+                _pageIndex < contents.length - 1
+                    ? 'NEXT'
+                    : 'GET STARTED', // Change the button text on the last screen
+
+                style: const TextStyle(fontSize: 18),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          )
+        ],
       ),
     );
   }
